b9e21d06c1e50694e47d0af681c72e49e0a0331c
Benjamin Lin <blin432@gmail.com>
2020-12-17 11:45:10 -0500

    added third set with data-structures

---
+++ javascript-exercises/third-set/15_qfroms.js
@@ -0,0 +1,30 @@
+// --- Directions
+// Implement a Queue datastructure using two stacks.
+// *Do not* create an array inside of the 'Queue' class.
+// Queue should implement the methods 'add', 'remove', and 'peek'.
+// For a reminder on what each method does, look back
+// at the Queue exercise.
+// --- Examples
+//     const q = new Queue();
+//     q.add(1);
+//     q.add(2);
+//     q.peek();  // returns 1
+//     q.remove(); // returns 1
+//     q.remove(); // returns 2
+
+const Stack = require('./data_structures/stack');
+
+class Queue {
+	
+	
+	
+}
+
+// const q = new Queue();
+// q.add(1);
+// q.add(2);
+// console.log(q.peek());  // returns 1
+// console.log(q.remove()); // returns 1
+// console.log(q.remove()); // returns 2
+
+module.exports = Queue;
---
+++ javascript-exercises/third-set/16_midpoint.js
@@ -0,0 +1,27 @@
+// --- Directions
+// Return the 'middle' node of a linked list.
+// If the list has an even number of elements, return
+// the node at the end of the first half of the list.
+// *Do not* use a counter variable, *do not* retrieve
+// the size of the list, and only iterate
+// through the list one time.
+// --- Example
+//   const l = new LinkedList();
+//   l.insertLast('a')
+//   l.insertLast('b')
+//   l.insertLast('c')
+//   midpoint(l); // returns { data: 'b' }
+
+const { LinkedList } = require('./data_structures/linked_list');
+
+function midpoint(list){
+	
+}
+
+let list = new LinkedList();
+list.insertLast('a');
+list.insertLast('b');
+list.insertLast('c');
+list.insertLast('d');
+
+console.log(midpoint(list));
